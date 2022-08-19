b513f58baa37a42874b7c71241634e6ca1722649
Jay7221 <jns29072003@gmail.com>
2022-08-19 12:12:16 +0530

    Modifications to list projects

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<string.h>
 #include<vector>
-const std::string TEMPLATE_PATH = "./Templates";
-const std::string PROJECT_PATH = "./Projects";
+using namespace std;
+const string TEMPLATE_PATH = "./Templates";
+const string PROJECT_PATH = "./Projects";
 
-int run_command(std::string command){
+int run_command(string command){
 	return system(command.c_str());
 }
 
@@ -15,9 +16,9 @@ class Project;
 // ---------------------------------- Class Definitions ---------------------------------------------
 class Manager{
 private:
-	std::string MAIN_PATH;
-	std::vector<Project*> PROJECTS;
-	std::vector<Template*> TEMPLATES;
+	string MAIN_PATH;
+	vector<Project*> PROJECTS;
+	vector<Template*> TEMPLATES;
 	bool IS_RUNNING;
 	int choice;
 	enum{
@@ -43,36 +44,36 @@ public:
 
 class Template{
 	private:
-		std::string name;
+		string name;
 		// the name of the project
-		std::string path;
+		string path;
 		// the location of the project
 	public:
-		Template(std::string template_name);
+		Template(string template_name);
 		// loads template with name "name" in the TEMPLATE_PATH folder
 		void open();
 		// opens the template
-		std::string getName();
+		string getName();
 		// returns the name of the template
-		std::string getPath();
+		string getPath();
 		// returns the path of the temlate
 };
 class Project{
 	private:
-		std::string path;
+		string path;
 		// the location of the project
-		std::string name;
+		string name;
 		// the name of the project
 		Template* projectTemplate;
 		// the template used for creating this project
 	public:
-		Project(std::string projectName);
+		Project(string projectName);
 		// Initialzes a project with name projectName and creates a folder in the current directory
-		Project(std::string projectName, Template* projectTemplate);
+		Project(string projectName, Template* projectTemplate);
 		// Initialzes a project with name projectName and creates a folder in the current directory using the template useTemplate
-		std::string getPath();
+		string getPath();
 		// Returns the location of the project
-		std::string getName();
+		string getName();
 		// Returns the name of the project
 		void loadTemplate(Template* projectTemplate);
 		// Copies all the files in the template useTemplate to the project
@@ -98,89 +99,90 @@ bool Manager::is_running(){
 
 bool Manager::init(){
 	freopen("error.log", "w", stderr);
-	std::cout << "************************ WELCOME TO PROJECT MANAGEER ************************\n";
+	cout << "************************ WELCOME TO PROJECT MANAGEER ************************" << endl;
 	choice = 0;
 	return 1;
 }
 void Manager::ListProjects(){
+	cout << "--------------------------------\n";
 	run_command("ls " + PROJECT_PATH);
+	cout << "---------------------------------\n";
 }
 void Manager::ListProjectTemplates(){
+	cout << "--------------------------------\n";
 	run_command("ls " + TEMPLATE_PATH);
+	cout << "---------------------------------\n";
 }
 void Manager::OpenProject(){
-	std::cout << "Enter the name of project to open : " << std::endl;
-	std::string name;
-	std::cin >> name;
+	cout << "Enter the name of project to open : " << endl;
+	string name;
+	cin >> name;
 	Project p(name);
 	p.open();
 	return ;
 }
 void Manager::OpenProjectTemplate(){
-	std::cout << "Enter the name of template to open : " << std::endl;
-	std::string name;
-	std::cin >> name;
+	cout << "Enter the name of template to open : " << endl;
+	string name;
+	cin >> name;
 	Template cur_template(name);
 	cur_template.open();
 }
 void Manager::CreateNewProject(){
-	std::cout << "Enter the name of the new project : " << std::endl;
-	std::string name;
-	std::cin >> name;
-	std::cout << std::endl;
-	std::cout << "Do you wish to use a template ? (Y / N) : " << std::endl;
+	cout << "Enter the name of the new project : " << endl;
+	string name;
+	cin >> name;
+	cout << endl;
+	cout << "Do you wish to use a template ? (Y / N) : " << endl;
 	char flag;
-	std::cin >> flag;
+	cin >> flag;
 	if(flag == 'Y' || flag == 'y'){
-		std::cout << "Enter the name of the template : " << std::endl;
-		std::string template_name;
-		std::cin >> template_name;
+		cout << "Enter the name of the template : " << endl;
+		string template_name;
+		cin >> template_name;
 		PROJECTS.push_back(new Project(name, new Template(template_name)));
 	}else if(flag == 'N' || flag == 'n'){
 		PROJECTS.push_back(new Project(name));
 	}else{
-		std::cout << "Unknown Command : " << flag << std::endl;
+		cout << "Unknown Command : " << flag << endl;
 		return ;
 	}
-	std::cout << "Project created with index : " << PROJECTS.size() - 1 << std::endl;
-	std::cout << name << std::endl;
+	cout << "Project created : " << name << endl;
 }
 void Manager::run(){
-	while(choice != EXIT){
-		std::cout << "\n\n\n\n\n" << std::endl;
-		std::cout << "Please select one of the below actions : " << std::endl;
-		std::cout << "0. View Projects " << std::endl;
-		std::cout << "1. Open existing project " << std::endl;
-		std::cout << "2. Create new project " << std::endl;
-		std::cout << "3. View project templates " << std::endl;
-		std::cout << "4. Open project template " << std::endl;
-		std::cout << "5. Exit" << std::endl;
-		std::cin >> choice;
-		switch(choice){
-			case VIEW_PROJECTS:
-				ListProjects();
-				break;
-			case OPEN_PROJECT:
-				ListProjects();
-				OpenProject();
-				Exit();
-				break ;
-			case CREATE_PROJECT:
-				CreateNewProject();
-				break ;
-			case VIEW_TEMPLATES:
-				ListProjectTemplates();
-				break ;
-			case OPEN_TEMPLATE:
-				ListProjectTemplates();
-				OpenProjectTemplate();
-				break ;
-			case EXIT:
-				Exit();
-				break ;
-			default:
-				std::cout << "Unknown option : " << choice << std::endl;
-		}
+	fflush(stdout);
+	cout << "\n\n\n\n\n" << endl;
+	cout << "Please select one of the below actions : " << endl;
+	cout << "0. View Projects " << endl;
+	cout << "1. Open project " << endl;
+	cout << "2. Create new project " << endl;
+	cout << "3. View project templates " << endl;
+	cout << "4. Open project template " << endl;
+	cout << "5. Exit" << endl;
+	cin >> choice;
+	switch(choice){
+		case VIEW_PROJECTS:
+			ListProjects();
+			break;
+		case OPEN_PROJECT:
+			ListProjects();
+			OpenProject();
+			break ;
+		case CREATE_PROJECT:
+			CreateNewProject();
+			break ;
+		case VIEW_TEMPLATES:
+			ListProjectTemplates();
+			break ;
+		case OPEN_TEMPLATE:
+			ListProjectTemplates();
+			OpenProjectTemplate();
+			break ;
+		case EXIT:
+			Exit();
+			break ;
+		default:
+			cout << "Unknown option : " << choice << endl;
 	}
 }
 void Manager::Exit(){
@@ -188,7 +190,7 @@ void Manager::Exit(){
 }
 
 //-------------------------------------------------------------------------------------------------------------
-Template::Template(std::string template_name)
+Template::Template(string template_name)
 {
 	// loads the template at path
 	name = template_name;
@@ -199,47 +201,47 @@ void Template::open(){
 	// opens the template
 	run_command("code " + path);
 }
-std::string Template::getName(){
+string Template::getName(){
 	// returns the name of the template
 	return name;
 }
-std::string Template::getPath(){
+string Template::getPath(){
 	// returns the path of the temlate
 	return path;
 }
 
 //------------------------------------------------------------------------------------
 
-Project::Project(std::string projectName){
+Project::Project(string projectName){
 	// Initialzes a project with name projectName and creates a folder in the current directory
 	name = projectName;
 	path = PROJECT_PATH + "/" + name;
 	run_command("mkdir " + path);
 }
 
-Project::Project(std::string projectName, Template* projectTemplate){
+Project::Project(string projectName, Template* projectTemplate){
 	// Initialzes a project with name projectName and creates a folder in the current directory using the template useTemplate
 	name = projectName;
 	path = PROJECT_PATH + "/" + name;
 	run_command("mkdir " + path);
 	run_command("cp -r " + projectTemplate -> getPath() + "/*" + " " + path);
 }
-std::string Project::getPath(){
+string Project::getPath(){
 	// Returns the location of the project
 	return path;
 }
-std::string Project::getName(){
+string Project::getName(){
 	// Returns the name of the project
 	return name;
 }
 void Project::loadTemplate(Template* projectTemplate){
 	// Copies all the files in the template useTemplate to the project
-	std::string command = "cp -r " + (projectTemplate -> getPath()) + " " + path;
+	string command = "cp -r " + (projectTemplate -> getPath()) + " " + path;
 	system(command.c_str());
 }
 void Project::deleteProject(){
 	// Deletes the project completely, also removing the corresponding directory.
-	std::string command = "rm " + path;
+	string command = "rm " + path;
 	system(command.c_str());
 }
 
@@ -262,7 +264,7 @@ void clear_screen(){
 int main(int argc, char* argv[]){
 	Manager M;
 	if(!M.init()){
-		std::cout << "Something Went Wrong" << std::endl;
+		cout << "Something Went Wrong" << endl;
 		return 0;
 	}
 
